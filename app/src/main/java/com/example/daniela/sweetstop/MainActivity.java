13a1402b35acbca307aa434344c5ac092c1fb75a
Gonzalo Condori Villca <gonzalocondorivillca@gmail.com>
2016-10-12 05:59:38 -0400

    adicion de fragmentos

--- app/src/main/java/com/example/daniela/sweetstop/MainActivity.java
+++ app/src/main/java/com/example/daniela/sweetstop/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.daniela.sweetstop;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TableLayout;
+
+import com.example.daniela.sweetstop.adapter.ViewPagerAdapter;
+import com.example.daniela.sweetstop.fragment.CategoriaFragment;
+import com.example.daniela.sweetstop.fragment.PedidoFragment;
+import com.example.daniela.sweetstop.fragment.ReservaFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewpager;
+    TabLayout tabLayout;
+    int[] tabIcons = {
+            R.drawable.ic_sillon,
+            R.drawable.ic_menu,
+            R.drawable.ic_pedidos
+    };
+    View iconSillon, iconMenu, iconPedidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +33,37 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        iconSillon = getLayoutInflater().inflate(R.layout.custom_icon_tab, null);
+        iconSillon.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_sillon);
 
+        iconMenu = getLayoutInflater().inflate(R.layout.custom_icon_tab, null);
+        iconMenu.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_menu);
+
+        iconPedidos = getLayoutInflater().inflate(R.layout.custom_icon_tab, null);
+        iconPedidos.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_pedidos);
+
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewpager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        assert tabLayout != null;
+        tabLayout.setupWithViewPager(viewpager);
+        setupTabIcons();
+        tabLayout.getTabAt(1).select();
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PedidoFragment(), "Pedido");
+        adapter.addFragment(new CategoriaFragment(), "Categoria");
+        adapter.addFragment(new ReservaFragment(), "Reserva");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setCustomView(iconPedidos);
+        tabLayout.getTabAt(1).setCustomView(iconMenu);
+        tabLayout.getTabAt(2).setCustomView(iconSillon);
     }
 }
---
+++ app/src/main/java/com/example/daniela/sweetstop/fragment/CategoriaFragment.java
@@ -0,0 +1,25 @@
+package com.example.daniela.sweetstop.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daniela.sweetstop.R;
+
+/**
+ * Created by gonzalopro on 10/12/16.
+ */
+
+public class CategoriaFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_categoria,container,false);
+
+        return root;
+    }
+}
